da95d2a53997e1df37f408cc206953842c7e5a40
Samson063 <furthermaths078@gmail.com>
2024-04-20 01:34:21 +0100

    Used the data in products.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,33 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        star: 4.5,
-        count: 87
-    },
-    priceInCent: 1090 
-},
-
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        star: 4,
-        count: 127
-    },
-    priceInCent: 2095
-},
-
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        star: 4.5,
-        count: 56
-    },
-    priceInCent: 799
-} ]
-
 let productsHTML = ''
 
 products.forEach( (product) => {
@@ -44,14 +14,14 @@ products.forEach( (product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.star * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceInCent / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
