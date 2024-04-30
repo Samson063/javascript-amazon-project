3585caa3599d181fce0476984635a253decb311a
Samson063 <furthermaths078@gmail.com>
2024-04-30 03:31:03 +0100

    moved addToCart into cart.js

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML = '';
@@ -55,40 +55,27 @@ products.forEach( (product) => {
         </div>`
 })
 
-
 document.querySelector('.js-product-grid').innerHTML = productsHTML;
+
+
+function updateCartQuantity(productId) {
+  let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+}
 
 document.querySelectorAll('.js-add-to-cart-btn')
 .forEach((button) => {
   button.addEventListener('click', () => {
     const productId = button.dataset.productId;
+    addToCart(productId);
+    updateCartQuantity();
 
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item
-      }
-    })
-
-    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-
-    const cartQuantity = quantitySelector.value;
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1
-      });
-    }
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 
 });
